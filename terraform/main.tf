bf0e809b77121c1d006d42abaf7f3a0864cd66a8
Vignesh Vaidyanathan <vignesh.vaid@gmail.com>
2023-01-24 22:21:22 -0800

    Add playbook for postgres

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,58 @@
-resource "hcloud_server" "postgres" {
-  name        = "postgres"
-  image       = "debian-11"
-  server_type = "cpx21"
-  location    = "hil"
+resource "hcloud_ssh_key" "vignesh" {
+  name = "vignesh"
+  public_key = <<EOT
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKfrJNa6zj4eiTc3BIadsB+kMcUT2pIf+oapRNcx35lp vignesh.vaid@gmail.com
+EOT
 }
+
+resource "hcloud_firewall" "postgres-server" {
+  name = "postgres-server"
+
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "80"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_server" "postgres-server" {
+  image       = "debian-11"
+  name        = "postgres-server"
+  location    = var.location
+
+  server_type = "cpx11"
+
+  ssh_keys = [
+    hcloud_ssh_key.vignesh.name
+  ]
+
+  firewall_ids = [
+    hcloud_firewall.postgres-server.id
+  ]
+}
+
+// Use hcloud_server_network for mastodon-server to postgres-server connection
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
 variable "hcloud_token" {
   type = string
 }
+
+variable "location" {
+  type = string
+  default = "hil"
+}
