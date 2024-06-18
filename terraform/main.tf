bf8d18c29bba638956f99a86a840983e35724bdb
Vignesh Vaidyanathan <vignesh.vaid@gmail.com>
2024-06-17 19:38:07 -0700

    Add a hole in port 5138 for SSH

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,13 +5,23 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKfrJNa6zj4eiTc3BIadsB+kMcUT2pIf+oapRNcx35lp
 EOT
 }
 
-resource "hcloud_firewall" "postgres-server" {
-  name = "postgres-server"
+resource "hcloud_firewall" "bastion" {
+  name = "bastion"
 
   rule {
     direction = "in"
     protocol  = "tcp"
     port      = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "5138"
     source_ips = [
       "0.0.0.0/0",
       "::/0"
@@ -39,9 +49,10 @@ resource "hcloud_firewall" "postgres-server" {
   }
 }
 
-resource "hcloud_server" "postgres-server" {
-  image    = "debian-11"
-  name     = "postgres-server"
+resource "hcloud_server" "bastion" {
+  // Debian 12 x86
+  image    = "114690387"
+  name     = "bastion"
   location = var.location
 
   server_type = "cpx11"
@@ -51,8 +62,8 @@ resource "hcloud_server" "postgres-server" {
   ]
 
   firewall_ids = [
-    hcloud_firewall.postgres-server.id
+    hcloud_firewall.bastion.id
   ]
-}
 
-// Use hcloud_server_network for mastodon-server to postgres-server connection
+  user_data = file("${path.module}/user_data.yml")
+}
